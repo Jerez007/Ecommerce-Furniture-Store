008e05f8a43be7949a2d69b22e9456234a34a736
Jerez Solis <team@vinple.com>
2021-03-09 20:29:32 -0800

    added hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import hero from '../assets/hero.jpg'
+import styled from "styled-components";
+
 
 const Hero = () => {
   return (
-    <div>
-      this is the hero
-    </div>
-  )
+    <Wrapper>
+      <div className="hero-center">
+        <img src={hero} alt="" />
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  img {
+    width: 100vw;
+  }
+
+  @media screen and (min-width: 1136px) {
+    .hero-center {
+      display: flex;
+      img {
+        width: 90vw;
+        margin: auto auto;
+      }
+    }
+  }
+`;
 
 export default Hero

d28da9b777381584f448d4ba4417636a702eee45
Jerez S <>
2021-04-06 23:43:18 -0700

    updated inspiration component

--- client/src/components/Inspiration.js
+++ client/src/components/Inspiration.js
@@ -14,20 +14,25 @@ const Inspiration = () => {
             <img src={image1} alt="furniture" />
             <h2>RESTYLE YOUR BEDROOM</h2>
             <p>
-              {
-                "The bedroom often ends up being a place where you store things and personal items that don’t belong anywhere else. Multiple storage solutions, such as underbed boxes, boxes and baskets make it easy to create a place for everything from extra towels, throws, shoes, odds and ends".substring(0, 200) + "..."
-              }
+              {"The bedroom often ends up being a place where you store things and personal items that don’t belong anywhere else. Multiple storage solutions, such as underbed boxes, boxes and baskets make it easy to create a place for everything from extra towels, throws, shoes, odds and ends".substring(
+                0,
+                200
+              ) + "..."}
             </p>
+
+            <button type="button">Read More</button>
           </div>
 
           <div className="right-content">
             <img src={image2} alt="furniture" />
             <h2>WHAT IS HYGGE?</h2>
             <p>
-              If you're not familiar with the Scandinavian concept of "Hygge"
-              (hoo•ga), Hygge is about creating a happy and cozy atmosphere in
-              your home.
+              {"If you're not familiar with the Scandinavian concept of 'Hygge' (hoo•ga), Hygge is about creating a happy and cozy atmosphere in your home. Hygge can be around the dinner table with friends and family or simply snuggling up in your favourite throw for a good read and a cup of coffee in your own company. Even something as simple as lit candles on your window sill can be hygge.".substring(
+                0,
+                200
+              ) + "..."}
             </p>
+            <button type="button">Read More</button>
           </div>
         </div>
       </div>
@@ -66,6 +71,23 @@ const Container = styled.div`
       margin-top: 20px;
       margin-bottom: 30px;
       font-size: 18px;
+    }
+
+    button {
+      width: 100px !important;
+      height: 40px !important;
+      border-color: var(--clr-primary-3);
+    }
+  }
+
+  /* media queries */
+  @media screen and (max-width: 769px) {
+    .right-content {
+      display: none;
+    }
+
+    .left-content {
+      width: 90vw !important;
     }
   }
 `;
